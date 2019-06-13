9873ec4d38cfd3d2d1553f9b72222902ec8f0e8d
Matt <lawrencm@gmail.com>
2019-06-13 10:22:25 +0800

    Adds files to the project file

--- listwidget/index.js
+++ listwidget/index.js
@@ -3,7 +3,7 @@ var Generator = require('yeoman-generator'),
   xml2js = require('xml2js'),
   VsUtils = require('../utils/vsUtils'),
   SfUtils = require('../utils/sfUtils'),
- getYoRcPath  = require('get-yo-rc-path'),
+  getYoRcPath = require('get-yo-rc-path'),
   _ = require('lodash');
 
 
@@ -33,14 +33,14 @@ module.exports = class extends Generator {
     var creds = this.config.get("promptValues");
 
     return [
-      ['Models/Widgets/Model.cs', `Application/Models/Widgets/${creds.widgetName}/${creds.widgetName}Model.cs`],
-      ['Models/Widgets/ViewModel.cs', `Application/Models/Widgets/${creds.widgetName}/${creds.widgetName}ViewModel.cs`],
-      ['Models/Interfaces/IModel.cs', `Application/Models/Interfaces/I${creds.widgetName}Model.cs`],
-      ['Controllers/Controller.cs', `Application/Controllers/Widgets/${creds.widgetName}Controller.cs`],
-      ['Views/Default.cshtml', `ResourcePackages/${creds.resourcePackage}/MVC/Views/${creds.widgetName}/${creds.widgetName}.Default.cshtml`],
-      ['Views/DesignerView.Simple.cshtml', `ResourcePackages/${creds.resourcePackage}/MVC/Views/${creds.widgetName}/DesignerView.Simple.cshtml`],
-      ['Views/DesignerView.Simple.json', `ResourcePackages/${creds.resourcePackage}/MVC/Views/${creds.widgetName}/DesignerView.Simple.json`],
-      ['Scripts/designerview-simple.js', `ResourcePackages/${creds.resourcePackage}/MVC/Scripts/${creds.widgetName}/designerview-simple.js`]
+      ['Models/Widgets/Model.cs', `Application/Models/Widgets/${creds.widgetName}/${creds.widgetName}Model.cs`, 'Compile'],
+      ['Models/Widgets/ViewModel.cs', `Application/Models/Widgets/${creds.widgetName}/${creds.widgetName}ViewModel.cs`, 'Compile'],
+      ['Models/Interfaces/IModel.cs', `Application/Models/Interfaces/I${creds.widgetName}Model.cs`, 'Compile'],
+      ['Controllers/Controller.cs', `Application/Controllers/Widgets/${creds.widgetName}Controller.cs`, 'Compile'],
+      ['Views/Default.cshtml', `ResourcePackages/${creds.resourcePackage}/MVC/Views/${creds.widgetName}/${creds.widgetName}.Default.cshtml`, 'Content'],
+      ['Views/DesignerView.Simple.cshtml', `ResourcePackages/${creds.resourcePackage}/MVC/Views/${creds.widgetName}/DesignerView.Simple.cshtml`, 'Content'],
+      ['Views/DesignerView.Simple.json', `ResourcePackages/${creds.resourcePackage}/MVC/Views/${creds.widgetName}/DesignerView.Simple.json`, 'Content'],
+      ['Scripts/designerview-simple.js', `ResourcePackages/${creds.resourcePackage}/MVC/Scripts/${creds.widgetName}/designerview-simple.js`, 'Content']
 
     ]
   }
@@ -81,13 +81,13 @@ module.exports = class extends Generator {
         type: 'list',
         name: 'resourcePackage',
         store: true,
-        message: 'What resource pAckge should this belong to',
+        message: 'What resource packge should this belong to',
         choices: await this.vsUtils.getResourcePackages(),
 
       }
     ])
 
-    
+
 
 
   }
@@ -101,11 +101,14 @@ module.exports = class extends Generator {
     var prompts = this.config.get("promptValues");
 
     var data = {
-        assemblyName: `${prompts.assemblyName}`,
-        widgetName: `${prompts.widgetName}`,
+      assemblyName: `${prompts.assemblyName}`,
+      widgetName: `${prompts.widgetName}`,
     }
 
 
+
+    var prjCompileStrings = [];
+    var prjContentStrings = [];
 
     this._private_getFiles().forEach(fileConf => {
       this.fs.copyTpl(
@@ -113,34 +116,82 @@ module.exports = class extends Generator {
         this.destinationPath(fileConf[1]),
         data
       );
+
+      //append to prjString
+      console.log(fileConf[2])
+      if (fileConf[2] == "Compile") {
+        prjCompileStrings.push(`<Compile Include="${fileConf[1].replace(/\//g, "\\")}" />`)
+      } else if (fileConf[2] == "Content") {
+        prjContentStrings.push(`<Content Include="${fileConf[1].replace(/\//g, "\\")}" />`)
+      }
+
     });
+
+    console.log(prjCompileStrings)
+    console.log(prjContentStrings)
 
 
     //update the dependency injection file
     //var path = "/path/to/file.html",
     getYoRcPath.dir().then((yoRcDir) => {
 
-      console.log(yoRcDir)
-      var path = `${yoRcDir}\\Application\\DI\\Modules\\ModelsModule.cs` 
- this.fs.copy(path, path, {
-      process: (content)=> {
-  
+      //append to the Denpendency INjector
+      var diPath = `${yoRcDir}\\Application\\DI\\Modules\\ModelsModule.cs`
+      this.fs.copy(diPath, diPath, {
+        process: (content) => {
           var newString = `
           
             Bind<I${data.widgetName}Model>().To<${data.widgetName}Model>();
 
             //yeoman insert above
 
-          `;        
-
+          `;
           var regEx = new RegExp('//yeoman insert above', 'g');
           var newContent = content.toString().replace(regEx, newString);
           return newContent;
-      }
-  });
-      // => yoRcPath = '/user/home'
-    });    
-   
+        }
+      });
+
+
+      // include files in project file
+      var prjPath = `${yoRcDir}\\SitefinityStarterProject.Website.csproj`
+      this.fs.copy(prjPath, prjPath, {
+        process: (content) => {
+
+          var newCompileString = `
+          
+          <!-- start of ${data.widgetName} compile includes -->
+          ${prjCompileStrings.join('\n')}
+          <!-- end of ${data.widgetName} compile includes -->
+          
+          <!-- END YEOMAN COMPILE INCLUDES -->
+          `
+
+
+          var newContentString = `
+          
+          <!-- start of ${data.widgetName} content includes -->
+          ${prjContentStrings.join('\n')}
+          <!-- end of ${data.widgetName} content includes -->
+          
+          <!-- END YEOMAN CONTENT INCLUDES -->
+          `
+
+
+          var cplRegEx = new RegExp('<!-- END YEOMAN COMPILE INCLUDES -->', 'g');
+          var cntRegEx = new RegExp('<!-- END YEOMAN CONTENT INCLUDES -->', 'g');
+
+          var newContent = content.toString().replace(cplRegEx, newCompileString).replace(cntRegEx, newContentString);;
+          return newContent;
+        }
+      });
+
+
+
+
+
+    });
+
 
   }
 };
